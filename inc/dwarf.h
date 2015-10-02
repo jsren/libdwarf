0939935d927b1156ad6e2865df489dbacb196fc5
James Renwick <jsrenwick3@gmail.com>
2015-10-02 18:22:09 +0100

    Further constants added.

--- inc/dwarf.h
+++ inc/dwarf.h
@@ -108,4 +108,129 @@ namespace dwarf
         Description         // Artificial name or description
     };
 
+    enum class BaseType : uint8_t
+    {
+        Address        = 0x01,
+        Boolean        = 0x02,
+        ComplexFloat   = 0x03,
+        Float          = 0x04,
+        Signed         = 0x05,
+        SignedChar     = 0x06,
+        Unsigned       = 0x07,
+        UnsignedChar   = 0x08,
+        ImaginaryFloat = 0x09,
+        PackedDecimal  = 0x0A,
+        NumericString  = 0x0B,
+        Edited         = 0x0C,
+        SignedFixed    = 0x0D,
+        UnsignedFixed  = 0x0E,
+        DecimalFloat   = 0x0F,
+        UTF            = 0x10
+    };
+
+    enum class DecimalSign : uint8_t
+    {
+        Unsigned          = 0x01,
+        LeadingOverpunch  = 0x02,
+        TrailingOverpunch = 0x03,
+        LeadingSeparate   = 0x04,
+        TrailingSeparate  = 0x05
+    };
+
+    enum class Endianness : uint8_t  // Dubbed "endianity" in spec
+    {
+        Default = 0x00,
+        Big     = 0x01,
+        Little  = 0x02
+    };
+
+    enum class Accessibility : uint8_t
+    {
+        Public    = 0x01,
+        Protected = 0x02,
+        Private   = 0x03
+    };
+
+    enum class Visibility : uint8_t
+    {
+        Local     = 0x01,
+        Exported  = 0x02,
+        Qualified = 0x03
+    };
+
+    enum class Virtuality : uint8_t
+    {
+        None        = 0x00,
+        Virtual     = 0x01,
+        PureVirtual = 0x02
+    };
+
+    enum class Language : uint16_t
+    {
+        C89          = 0x00,
+        C            = 0x02,
+        Ada83        = 0x03,
+        CPlusPlus    = 0x04,
+        Cobol74      = 0x05,
+        Cobol85      = 0x06,
+        Fortran77    = 0x07,
+        Fortran90    = 0x08,
+        Pascal83     = 0x09,
+        Modula2      = 0x0A,
+        Java         = 0x0B,
+        C99          = 0x0C,
+        Ada95        = 0x0D,
+        Fortran95    = 0x0E,
+        PLI          = 0x0F,
+        ObjC         = 0x10,
+        ObjCPlusPlus = 0x11,
+        UPC          = 0x12,
+        D            = 0x13,
+        Python       = 0x14
+    };
+
+    enum class IdentifierCase : uint8_t
+    {
+        CaseSensitive   = 0x00,
+        UpCase          = 0x01,
+        DownCase        = 0x02,
+        CaseInsensitive = 0x03
+    };
+
+    enum class CallingConvention : uint8_t
+    {
+        Normal  = 0x01,
+        Program = 0x02,
+        Nocall  = 0x03
+    };
+
+    enum class Inlining : uint8_t
+    {
+        NotInlined         = 0x00,
+        Inlined            = 0x01,
+        DeclaredNotInlined = 0x02,
+        DeclaredInlined    = 0x03
+    };
+
+    enum class ArrayOrdering : uint8_t
+    {
+        RowMajor = 0x0,
+        ColMajor = 0x1
+    };
+
+    enum class DiscriminantList : uint8_t
+    {
+        Label = 0x0,
+        Range = 0x1
+    };
+
+    enum class MacroInfoType : uint8_t
+    {
+        Define    = 0x01,
+        Undef     = 0x02,
+        StartFile = 0x03,
+        EndFile   = 0x04,
+        VendorExt = 0xFF
+    };
+
 }
---
+++ inc/format.h
@@ -0,0 +1,159 @@
+#pragma once
+#include <stdint.h>
+
+namespace dwarf
+{
+    enum class OpCode : uint8_t
+    {
+        Address = 0x03,
+        Deref   = 0x06,
+
+        Const1U = 0x08, // 1-byte unsigned constant
+        Const1S = 0x09, // 1-byte signed constant
+        Const2U = 0x0A, // 2-byte unsigned constant
+        Const2S = 0x0B, // 2-byte signed constant
+        Const4U = 0x0C, // 4-byte unsigned constant
+        Const4S = 0x0D, // 4-byte signed constant
+        Const8U = 0x0E, // 8-byte unsigned constant
+        Const8S = 0x0F, // 8-byte signed constant
+        ConstU  = 0x10, // ULEB128 constant
+        ConstS  = 0x11, // SLEB128 constant
+
+        Dup        = 0x12,
+        Drop       = 0x13,
+        Over       = 0x14,
+        Pick       = 0x15,
+        Swap       = 0x16,
+        Rot        = 0x17,
+        XDeref     = 0x18,
+        Abs        = 0x19,
+        And        = 0x1A,
+        Div        = 0x1B,
+        Minus      = 0x1C,
+        Mod        = 0x1D,
+        Mul        = 0x1E,
+        Neg        = 0x1F,
+        Not        = 0x20,
+        Or         = 0x21,
+        Plus       = 0x22,
+        PlusUConst = 0x23,
+        Shl        = 0x24,
+        Shr        = 0x25,
+        Shra       = 0x26,
+        Xor        = 0x27,
+        Skip       = 0x2F,
+        Bra        = 0x28,
+        Eq         = 0x29,
+        Ge         = 0x2A,
+        Gt         = 0x2B,
+        Le         = 0x2C,
+        Lt         = 0x2D,
+        Ne         = 0x2E,
+        //LitBase  = 0x30, // Literal values from 0 to 31 inclusive
+        //RegBase  = 0x50, // Register nos. from 0 to 31 inclusive
+        RegX       = 0x90,
+        FBReg      = 0x91,
+        BRegX      = 0x92,
+        Piece      = 0x93,
+        DerefSize  = 0x94,
+        XDerefSize = 0x95,
+        Nop        = 0x96,
+
+        PushObjectAddress = 0x97,
+        Call2             = 0x98,
+        Call4             = 0x99,
+        CallRef           = 0x9A,
+        FormTLSAddress    = 0x9B,
+        CallFrameCFA      = 0x9C,
+        BitPiece          = 0x9D,
+        ImplicitValue     = 0x9E,
+        StackValue        = 0x9F
+    };
+
+
+    /* Opcodes used in line number calculation */
+    enum class LineOpCode : uint8_t
+    {
+        Copy             = 0x01,
+        AdvancePC        = 0x02,
+        AdvanceLine      = 0x03,
+        SetFile          = 0x04,
+        SetColumn        = 0x05,
+        NegateStmnt      = 0x06,
+        SetBasicBlock    = 0x07,
+        ConstAddPC       = 0x08,
+        FixedAdvancePC   = 0x09,
+        SetPrologueEnd   = 0x0A,
+        SetEpilogueBegin = 0x0B,
+        SetISA           = 0x0C
+    };
+
+    /* Extended opcodes used in line number calculation */
+    enum class LineOpCodeEx : uint8_t
+    {
+        EndSequence      = 0x01,
+        SetAddress       = 0x02,
+        DefineFile       = 0x03,
+        SetDescriminator = 0x04
+    };
+
+
+#pragma region .DEBUG_PUBNAMES/.DEBUG_PUBTYPES
+
+    struct NameTableHeader32
+    {
+    public:
+        uint32_t unitLength;      // Length of the set of entries for this compilation unit, not including the 
+                                  // length field itself
+        uint16_t version;         // Version identifier containing the value 2
+        uint32_t debugInfoOffset; // Offset into the .debug_info section of the compilation unit header
+        uint32_t debugInfoLength; // Length containing the size in bytes of the contents of the .debug_info section
+                                  // generated to represent this compilation unit
+    } __packed__;
+
+    struct NameTableHeader64
+    {
+    public:
+        unsigned : 32;            // Should be 0xFFFFFFFF
+        uint64_t unitLength;      // Length of the set of entries for this compilation unit, not including the 
+                                  // length field itself
+        uint16_t version;         // Version identifier containing the value 2
+        uint64_t debugInfoOffset; // Offset into the .debug_info section of the compilation unit header
+        uint64_t debugInfoLength; // Length containing the size in bytes of the contents of the .debug_info section
+                                  // generated to represent this compilation unit
+    } __packed__;
+
+#pragma endregion
+
+
+#pragma region .DEBUG_ARRANGES
+
+    struct AddressRangeTableHeader32
+    {
+    public:
+        uint32_t unitLength;      // Length of the set of entries for this compilation unit, not including the 
+                                  // length field itself
+        uint16_t version;         // Version identifier containing the value 2
+        uint32_t debugInfoOffset; // Offset into the .debug_info section of the compilation unit header
+        uint8_t  addressSize;     // The size in bytes of an address (or the offset portion for segmented) on the target system
+        uint8_t  segmentSize;     // The size in bytes of a segment selector on the target system
+    } __packed__;
+
+    struct AddressRangeTableHeader64
+    {
+    public:
+        unsigned : 32;            // Should be 0xFFFFFFFF
+        uint64_t unitLength;      // Length of the set of entries for this compilation unit, not including the 
+                                  // length field itself
+        uint16_t version;         // Version identifier containing the value 2
+        uint64_t debugInfoOffset; // Offset into the .debug_info section of the compilation unit header
+        uint8_t  addressSize;     // The size in bytes of an address (or the offset portion for segmented) on the target system
+        uint8_t  segmentSize;     // The size in bytes of a segment selector on the target system
+    } __packed__;
+
+#pragma endregion
+
+
+
+
+}
